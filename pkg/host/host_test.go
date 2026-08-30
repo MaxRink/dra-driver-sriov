366e9bd322e36cbc883d8fc681efffad693f119c
Maximilian Rink <maximilian.rink@telekom.de>
2026-08-31 00:43:20 +0200

    fix: address native VF review findings

--- pkg/host/host_test.go
+++ pkg/host/host_test.go
@@ -1232,6 +1232,8 @@ vhost_net 32768 1 tun, Live 0xffffffffa0456000`),
 				}
 				err := hTest.ResetVF("eth0", testPtr(0), restore)
 				Expect(err).To(HaveOccurred())
+				Expect(err.Error()).To(ContainSubstring("reset vlan"))
+				Expect(err.Error()).To(ContainSubstring("reset link state"))
 			})
 		})
 	})
